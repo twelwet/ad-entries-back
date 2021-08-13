0db04b4a8d8bc494f1a9cdd2501fa6dd0753d36e
twelwet <akimovmax@mail.ru>
2021-08-13 20:45:00 +0300

    Добавил api-ответ '/essential'

--- src/local-methods/get-essential.js
+++ src/local-methods/get-essential.js
@@ -1,12 +1,9 @@
 'use strict';
 
+const moment = require('moment');
 const { readFromFile } = require('../utils');
-const { FileName, UserStatus } = require('../constants');
-
-const reducer = (accumulator, currentValue) => accumulator + currentValue;
-const getBoxSize = (bytes) => bytes / 1024 / 1024 / 1024;
-const isDisabled = (status) => UserStatus.DISABLED.find((item) => item === parseInt(status, 10));
-const isEnabled = (status) => UserStatus.ENABLED.find((item) => item === parseInt(status, 10));
+const { reducer, getBoxSize, isDisabled, isEnabled, getYears, mapEntriesToYears } = require('./utils');
+const { FileName } = require('../constants');
 
 const getEssential = async () => {
   try {
@@ -16,11 +13,24 @@ const getEssential = async () => {
     const allBoxesSize = usersWithEmails
       .filter((entry) => entry.user.person.emailBoxSize)
       .map((entry) => getBoxSize(entry.user.person.emailBoxSize)).reduce(reducer);
+
     const disabledUsers = allUsers.filter((entry) => isDisabled(entry.user.account.status));
     const enabledUsers = allUsers.filter((entry) => isEnabled(entry.user.account.status));
 
     const disabledEmails = usersWithEmails.filter((entry) => isDisabled(entry.user.account.status));
     const enabledEmails = usersWithEmails.filter((entry) => isEnabled(entry.user.account.status));
+
+    const userCreations = allUsers.map((entry) => moment(entry.objectInfo.whenCreated).format('YYYY'));
+    const emailCreations = usersWithEmails.map((entry) => moment(entry.objectInfo.whenCreated).format('YYYY'));
+
+    const yearsOfUsersCreation = getYears(userCreations);
+    const yearsOfEmailsCreation = getYears(emailCreations);
+
+    const userLastLogons = allUsers.map((entry) => entry.user.account.lastLogon === null ? 'never' : moment(entry.user.account.lastLogon).format('YYYY'));
+    const emailLastLogons = usersWithEmails.map((entry) => entry.user.account.lastLogon === null ? 'never' : moment(entry.user.account.lastLogon).format('YYYY'));
+
+    const yearsOfUsersLastLogons = getYears(userLastLogons);
+    const yearsOfEmailsLastLogons = getYears(emailLastLogons);
 
     return {
       accounts: {
@@ -28,13 +38,17 @@ const getEssential = async () => {
         enabled: enabledUsers.length,
         disabled: disabledUsers.length,
         withEmails: usersWithEmails.length,
+        creation: mapEntriesToYears(yearsOfUsersCreation, userCreations),
+        lastLogon: mapEntriesToYears(yearsOfUsersLastLogons, userLastLogons)
       },
       emails: {
+        allBoxesSize,
         all: usersWithEmails.length,
         enabled: enabledEmails.length,
         disabled: disabledEmails.length,
+        creation: mapEntriesToYears(yearsOfEmailsCreation, emailCreations),
+        lastLogon: mapEntriesToYears(yearsOfEmailsLastLogons, emailLastLogons),
       },
-      allBoxesSize
     };
 
   } catch(err) {
---
+++ src/local-methods/utils.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const {UserStatus} = require('../constants');
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+const getBoxSize = (bytes) => bytes / 1024 / 1024 / 1024;
+
+const isDisabled = (status) => UserStatus.DISABLED.find((item) => item === parseInt(status, 10));
+const isEnabled = (status) => UserStatus.ENABLED.find((item) => item === parseInt(status, 10));
+
+const getYears = (entries) => [...(new Set (entries))].sort();
+
+const mapEntriesToYears = (years, entries) => years
+  .map((year) => ({[year]: entries
+      .filter((entry) => entry === year).length}));
+
+module.exports = { reducer, getBoxSize, isDisabled, isEnabled, getYears, mapEntriesToYears };
